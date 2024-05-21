a45fedafe3c08b8dd655587eb5e34a7294b1756f
qhungo0125 <hungngojan25@gmail.com>
2024-05-21 23:55:03 +0700

    update

--- express_service/services/job/__tests__/updateJobApi.test.js
+++ express_service/services/job/__tests__/updateJobApi.test.js
@@ -7,27 +7,30 @@ describe('Start unit test for update job API', () => {
   let app;
   let server;
 
-  beforeAll(() => {
-    sequelize.authenticate();
-    app = createServer();
+  beforeAll(async () => {
+    await sequelize.authenticate();
+    app = await createServer();
     server = app.listen(5002); // Ensure the server is listening on a port
   });
 
-  afterAll(() => {
-    sequelize.close();
+  afterAll(async () => {
+    await sequelize.close();
     server.close(); // Close the server after tests
   });
 
-  test('data is OK, Should return status 200', async () => {
-    const jobId = '77rJraD'; // assuming jobId should be an integer
-    const job = jobs[0];
+  describe('Test CreateJob function()', () => {
+    test('data is OK, Should return status 200', async () => {
+      const jobId = '77rJraD'; // assuming jobId should be an integer
+      const job = jobs[0];
 
-    const response = await supertest(app).patch(`/${jobId}`).send(job).expect(200);
+      const response = await supertest(app).patch(`/${jobId}`).send(job).expect(200);
 
-    expect(response.body).toEqual({
-      statusCode: 200,
-      data: true,
-      message: 'OK',
+      expect(response.body).toEqual({
+        statusCode: 200,
+        data: true,
+        message: 'OK',
+      });
+      // expect(1).toBe(1);
     });
   });
 });
